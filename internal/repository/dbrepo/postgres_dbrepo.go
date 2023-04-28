3f85f70941da7a86347663df841074dd169ab5ef
Kurt Hakim <kurthakim@gmail.com>
2023-04-28 14:13:28 +0700

    Creating handlers to get movies for public and admin

--- internal/repository/dbrepo/postgres_dbrepo.go
+++ internal/repository/dbrepo/postgres_dbrepo.go
@@ -90,7 +90,7 @@ func (m *PostgresDBRepo) OneMovie(id int) (*models.Movie, error) {
 	}
 
 	// get genres, if any
-	query = `select g.id, g.genre from movies_genre mg
+	query = `select g.id, g.genre from movies_genres mg
     left join genres g on (mg.genre_id = g.id)
     where mg.movie_id = $1
     order by g.genre`
@@ -150,7 +150,7 @@ func (m *PostgresDBRepo) OneMovieForEdit(id int) (*models.Movie, []*models.Genre
 	}
 
 	// get genres, if any
-	query = `select g.id, g.genre from movies_genre mg
+	query = `select g.id, g.genre from movies_genres mg
     left join genres g on (mg.genre_id = g.id)
     where mg.movie_id = $1
     order by g.genre`
@@ -182,7 +182,7 @@ func (m *PostgresDBRepo) OneMovieForEdit(id int) (*models.Movie, []*models.Genre
 
 	var allGenres []*models.Genre
 
-	query = "select id, genre, from genres order by genre"
+	query = "select id, genre from genres order by genre"
 	gRows, err := m.DB.QueryContext(ctx, query)
 	if err != nil {
 		return nil, nil, err
